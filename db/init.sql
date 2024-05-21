1f5aa0df36c873066e024c60f975bb438ca5ce7b
Luiz Felipe <gilberto.barbosa@7comm.com.br>
2024-05-20 22:05:46 -0300

    FEAT: Initial

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS dbtasks;
+
+USE dbtasks;
+
+CREATE TABLE Task(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    task    VARCHAR(100)    NOT NULL,
+    dueDate DATETIME
+);
